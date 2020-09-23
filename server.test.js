9b547f5a51830b2b46b3c27244cd1c7212935a8f
mosesbenjamin <mavewreek@gmail.com>
2020-09-23 23:38:36 +0100

    Organizing tests with describe()

--- server.test.js
+++ server.test.js
@@ -3,22 +3,29 @@ const expect = require('expect');
 
 var app = require('./server').app;
 
-it('should return hello world response', (done)=>{
-    request(app)
-        .get('/')
-        .expect(404)
-        .expect((res)=>{
-            expect(res.body).toInclude({name: 'Example test.'})
-        })
-        .end(done)
-});
+describe('Server', ()=>{
 
-it('should return an array of users data', (done)=>{
-    request(app)
-    .get('/users')
-    .expect(200)
-    .expect((res)=>{
-        expect(res.body).toInclude({name: 'Mavewrick', age: 20})
+    describe('GET /', ()=>{
+        it('should return hello world response', (done)=>{
+            request(app)
+            .get('/')
+            .expect(404)
+            .expect((res)=>{
+                expect(res.body).toInclude({name: 'Example test.'})
+            })
+            .end(done);
+        });
     })
-    .end(done);
+    
+    describe('GET /users', ()=>{
+        it('should return an array of users data', (done)=>{
+            request(app)
+            .get('/users')
+            .expect(200)
+            .expect((res)=>{
+                expect(res.body).toInclude({name: 'Mavewrick', age: 20})
+            })
+            .end(done);
+        });
+    });
 });
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -2,31 +2,45 @@ const expect = require('expect');
 
 const utils = require('./utils');
 
-it('should add two numbers', ()=>{
-    var res = utils.add(33, 11);
+describe('Utils', ()=>{
 
-    expect(res).toBe(44).toBeA('number');
-});
+    describe('#add', ()=>{
+        it('should add two numbers', ()=>{
+            var res = utils.add(33, 11);
+        
+            expect(res).toBe(44).toBeA('number');
+        });
+        
+        it('should async add two numbers', (done)=>{
+            utils.asyncAdd(4, 3, (sum)=>{
+                expect(sum).toBe(7).toBeA('number');
+                done();
+            });
+        });
+    })
+    
+    describe('#square', ()=>{
+        it('should square a number', ()=>{
+            var res = utils.square(5);
+        
+            expect(res).toBe(25).toBeA('number');
+        });
+        
+        it('should async square a number', (done)=>{
+            utils.asyncSquare(8, (square)=>{
+                expect(square).toBe(64).toBeA('number');
+                done();
+            });
+        });
+    })
 
-it('should async add two numbers', (done)=>{
-    utils.asyncAdd(4, 3, (sum)=>{
-        expect(sum).toBe(7).toBeA('number');
-        done();
+    it('should verify first and last names are set', ()=>{
+        var user = {age: 20, location: 'Nigeria'};
+        var res = utils.setName(user, 'Moses Benjamin');
+
+        expect(res).toInclude({firstName: 'Moses', lastName: 'Benjamin'});
     });
-});
-
-it('should square a number', ()=>{
-    var res = utils.square(5);
-
-    expect(res).toBe(25).toBeA('number');
-});
-
-it('should async square a number', (done)=>{
-    utils.asyncSquare(8, (square)=>{
-        expect(square).toBe(64).toBeA('number');
-        done();
-    });
-});
+})
 
 // it('should expect some values', ()=>{
 //     // expect(12).toNotBe(12);
@@ -35,10 +49,3 @@ it('should async square a number', (done)=>{
 //     // expect([2, 3, 4]).toInclude(3);
 //     expect({name: 'Mavewrick', age: 20, location: 'Nigeria'}).toInclude({age: 20}).toExclude({location: 'Ghana'});
 // });
-
-it('should verify first and last names are set', ()=>{
-    var user = {age: 20, location: 'Nigeria'};
-    var res = utils.setName(user, 'Moses Benjamin');
-
-    expect(res).toInclude({firstName: 'Moses', lastName: 'Benjamin'});
-});
