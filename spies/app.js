2e4c14169972943e636180ac35d95aec07dbfab0
mosesbenjamin <mavewreek@gmail.com>
2020-09-23 23:58:22 +0100

    Test spies

---
+++ spies/app.js
@@ -0,0 +1,6 @@
+var db = require('./db.js');
+
+module.exports.handleSignup = (email, password)=>{
+
+    db.saveUser({email, password});
+}
---
+++ spies/app.test.js
@@ -0,0 +1,25 @@
+const expect = require('expect');
+const rewire = require('rewire');
+
+var app = rewire('./app.js');
+
+describe('App', ()=>{
+    var db = {
+        saveUser: expect.createSpy()
+    };
+    app.__set__('db', db);
+
+    it('should call the spy correctly', ()=>{
+        var spy = expect.createSpy();
+        spy('Mavewrick', 20);
+        expect(spy).toHaveBeenCalledWith('Mavewrick', 20);
+    })
+
+    it('should call saveUser with user object', ()=>{
+        var email = 'Mavewrick@mail.com';
+        var password = '123abc';
+
+        app.handleSignup(email, password);
+        expect(db.saveUser).toHaveBeenCalledWith({email, password});
+    })
+})
